38bbaea38a20a372a105b5f96827ea63dee4f356
Alan Nato <alanwnato@gmail.com>
2023-07-24 14:59:11 +0300

    fix : Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
-""" Script to get TODO list progress
-    by employee ID and save it to
-    CSV file
-"""
-from csv import writer, QUOTE_ALL
-from requests import get
-from sys import argv
+"""Exports data in the CSV format"""
 
+if __name__ == "__main__":
 
-def todo_csv(emp_id):
-    """ Send request for employee's
-        to do list to API
-    """
-    file_name = '{}.csv'.format(emp_id)
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    import csv
+    import requests
+    import sys
 
-    # check if user exists
-    user = get(url_user + emp_id).json().get('username')
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    if user:
-        params = {'userId': emp_id}
-        #  get all tasks
-        tasks = get(url_todo, params=params).json()
-        if tasks:
-            #  open file in write mode and use csv writer to
-            #  writer content
-            with open(file_name, 'w', newline='', encoding='utf8') as f:
-                task_writer = writer(f, quoting=QUOTE_ALL)
-                for task in tasks:
-                    task_writer.writerow([emp_id, user, task.get('completed'),
-                                         task.get('title')])
-
-
-if __name__ == '__main__':
-    if len(argv) > 1:
-        todo_csv(argv[1])
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])
