c6dcd2000f03c65337535efea630b66fc330bfc2
Alan Nato <alanwnato@gmail.com>
2023-08-08 10:04:02 +0000

    fix : How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
