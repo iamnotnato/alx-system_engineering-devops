d121addc51e4651b8f174c540d1b16d1558df066
Alan Nato <alanwnato@gmail.com>
2023-08-08 10:26:03 +0000

    feat : Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,46 +1,46 @@
 #!/usr/bin/python3
-""" Recursive API calls to Redit
-    and count occurrences
-"""
-from requests import get
+"""Module with top_ten function"""
+import re
+import requests
+import sys
 
 
-def count_words(subreddit, word_list, key_words={}, count={}, after=None):
-    """ get count of occurrence of words in word_list from
-        titles in hot articles in given subreddit
-    """
-    if not count:
-        key_words = {word.lower(): 0 for word in word_list}
-        word_list = [word.lower() for word in word_list]
-        count = {word.lower(): word_list.count(word.lower())
-                 for word in word_list}
+def count_words(subreddit, word_list, after="", to_print={}):
+    """Queries the Reddit API and returns a list containing the titles of all
+    hot articles for a given subreddit."""
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    params = {'after': after, 'limit': 100}
-    headers = {'user-agent': 'my-app/0.0.1'}
+    if after == "":
+        for word in word_list:
+            to_print[word] = 0
 
-    req = get(url, params=params, headers=headers, allow_redirects=False)
-    #  get data if request was successful
-    if req.status_code == 200:
-        data = req.json().get('data')
-        after = data.get('after')
-        posts = data.get('children')
+    url = "https://www.reddit.com/r/{}/hot.json{}".format(subreddit, after)
+    json_obj = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
 
-        #  get count of key words in each title
-        for post in posts:
-            title = post.get('data').get('title').lower()
-            words = title.split()
-            for word in key_words.keys():
-                key_words[word] += words.count(word)
+    if json_obj.status_code != 404:
 
-        #  call recursive function if there's more data
-        if after:
-            return count_words(subreddit, word_list, key_words, count, after)
+        dict_obj = json_obj.json()
+        list_obj = dict_obj.get('data').get('children')
+
+        for each in list_obj:
+            title = each.get('data').get('title')
+            tit_list = title.split()
+            for word in word_list:
+                c = re.compile(r"^{}$".format(word), re.I)
+                for each_tit_w in tit_list:
+                    res = c.findall(each_tit_w)
+                    to_print[word] += len(res)
+
+        next_fullname = dict_obj.get('data').get('after')
+        after = "?after={}".format(next_fullname)
+
+        if next_fullname is not None:
+            count_words(subreddit, word_list, after, to_print)
         else:
-            for key in key_words.keys():
-                key_words[key] *= count[key]
-            key_words = sorted(key_words.items(),
-                               key=lambda item: (-item[1], item[0]))
-            for item in key_words:
-                if item[1] > 0:
-                    print("{}: {}".format(item[0], item[1]))
+            sorted_l = sorted(to_print.items(), key=lambda x: x[1])
+            sorted_l.reverse()
+
+            for x in sorted_l:
+                if x[1] != 0:
+                    print("{}: {}".format(x[0], x[1]))
+
+    return None
